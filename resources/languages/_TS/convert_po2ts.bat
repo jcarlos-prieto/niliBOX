e95129a8aa77b37c861eb521648616bf48d41578
jcarlos-prieto <jcarlos.prieto@gmail.com>
2025-06-25 18:19:57 +0200

    Initial commit

--- resources/languages/_TS/convert_po2ts.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set QTDIR=C:\Qt\6.7.3\mingw_64
-
-for %%f in (*.po) do (
-    echo Converting %%f to %%~nf.ts
-    %QTDIR%\bin\lconvert.exe -if po -of ts -o "%%~nf.ts" "%%f"
-)
--- resources/languages/_TS/convert_ts2po.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set QTDIR=C:\Qt\6.7.3\mingw_64
-
-for %%f in (*.ts) do (
-    echo Converting %%f to %%~nf.po
-    %QTDIR%\bin\lconvert.exe -if ts -of po -o "%%~nf.po" "%%f"
-)
